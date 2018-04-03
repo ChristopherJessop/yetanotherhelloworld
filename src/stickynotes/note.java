1368ea668edcea50220cda564a4aa6feab6dd71c
Chris Jessop <jessopc@scientificsoftware.ca>
2018-04-03 16:54:25 -0600

    stub of note class
    Signed-off-by: Chris Jessop <jessopc@scientificsoftware.ca>

---
+++ src/stickynotes/note.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package stickynotes;
+
+/**
+ * @author jessopc
+ *
+ */
+public class note {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
